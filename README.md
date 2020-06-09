81091cc2126b8ce15aee70739be4e76f7459a945
Gerson Rodrigues <gersonrodrigues2078@gmail.com>
2020-06-09 08:14:44 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Projeto contaazul, com  erros nas urls acrescentando uma / a mais , e erro no inspecionador Chrome, não mnostra
+os registros do banco na aba network
---
+++ controllers/ajaxController.php
@@ -0,0 +1,32 @@
+<?php
+class ajaxController extends controller {
+
+	public function __construct() {
+        parent::__construct();
+
+        $u = new Users();
+        if($u->isLogged() == false) {
+        	header("Location: ".BASE_URL."login");
+        	exit;
+        }
+    }
+
+	public function index(){}
+
+    
+    public function search_clients() {
+    	$data = array();
+    	$u = new Users();
+        $u->setLoggedUser();
+    	$c = new Clients();
+
+    	if(isset($_GET['q']) && !empty($_GET['q'])) {
+    		$q = addslashes($_GET['q']);
+
+    		$clients = $c->searchClientByName($q, $u->getCompany());
+		}
+
+		echo json_encode($data);
+	}
+
+}
